e434e391a77d73c149a6b5a53bb6a188062aeadf
DESKTOP-3GT3IJ1\islab <t112598003@ntut.org.tw>
2025-05-04 21:21:05 +0800

    RAG Adjust

--- RAG.py
+++ RAG.py
@@ -11,7 +11,7 @@ class RAGPipeline:
             self.data = json.load(f)
         # 把房型資料串接起來作為 embedding corpus
         self.docs = [
-            Document(page_content=f"名稱:{item['name']} 價格:{item['price']} 面積:{item['area']} 特色:{item['features']} 風格:{item.get('style', '')} 房型:{item.get('maxOccupancy', '')}")
+            Document(page_content=f"名稱:{item['name']} 價格:{item['price']} 面積:{item['area']} 特色:{item['features']} 風格:{item.get('style', '')} 床數:{item.get('maxOccupancy', '')}")
             for item in self.data
         ]
         embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
@@ -19,14 +19,13 @@ class RAGPipeline:
         self.retriever = self.vectorstore.as_retriever(search_kwargs={"k": 10})
 
     def LLM_Prediction(self, question, rooms_summary):
-        llm = Ollama(model="llama3.2:latest", temperature=0.5)
+        llm = Ollama(model="llama3.2:latest", temperature=0.7)
         prompt = ChatPromptTemplate.from_messages([
             ("system",
-            "你是一個專業且親切的飯店推薦助手，請根據使用者的需求（例如價格、風格、幾人房）"
-            "從提供的房型資料中挑選出最符合需求的房型，最多只列出3個。"
-            "如果有多個符合需求的房型，請只列出前三個。"
-            "如果使用者的問題與房型推薦無關，請禮貌回覆「我是一個飯店推薦助手，目前只提供房型相關的建議喔！」。"
-            "不要編造資料庫中沒有的房型名稱，全程請務必使用繁體中文回覆。"),
+            "你是一個專業且親切的飯店推薦助手，請根據使用者的需求（例如價格、風格、幾人房）\n"
+            "請根據以上提供的房型資料中挑選出「最符合」客戶需求的房型，且最多只列出3個。\n"
+            "如果使用者的問題與房型推薦無關，請禮貌回覆「我是一個飯店推薦助手，目前只提供房型相關的建議喔！」。\n"
+            "不要編造資料庫中沒有的房型名稱，請務必使用「繁體中文」回覆。\n"),
             ("user",
             "使用者需求：{input}\n"
             "房型資料：{rooms}\n"
@@ -44,12 +43,15 @@ class RAGPipeline:
         result = chain.invoke({"input": question, "rooms": rooms_summary})
         return result
 
-    def query(self, question):
+    def getRoomSummaryByRAG(self,question):
         # 只用 retriever 撈 top10，不做 Python 過濾
         docs = self.retriever.get_relevant_documents(question)
         # 組成房型摘要文字
-        rooms_summary = "\n".join([doc.page_content for doc in docs])
+        return "\n".join([doc.page_content for doc in docs])
 
+
+    def query(self, question):
+        rooms_summary = self.getRoomSummaryByRAG(question)
         if rooms_summary.strip():
             conclusion = self.LLM_Prediction(question, rooms_summary)
             response = {
